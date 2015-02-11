346ff1f08f1b0885c81013a69c7cbea4849597c8
Ryan Millay <ryanmillay@Ryans-MacBook-Pro-2.local>
2015-02-10 19:09:26 -0500

    updated readme and driver

--- src/main/java/nu/cs6240/hadoop/a2/A2_Driver.java
+++ src/main/java/nu/cs6240/hadoop/a2/A2_Driver.java
@@ -19,19 +19,20 @@ public class A2_Driver {
 
 	public static void main(String[] args) throws Exception {
 		// let's verify the number of args
-		if(args.length < 5) {
-			System.err.println("Usage: <input path> <output path> <sampling rate> <# of bins> <use combiner? true|false>");
+		if(args.length < 4) {
+			System.err.println("Usage: <input path> <output path> <sampling rate> <# of bins> <combiner class>");
 			System.exit(-1);
 		}
 		
 		// parse the sampling rate and number of bins to verify their format
 		int samplingRate = 1;
 		int numBins = 1;
-		boolean useCombiner = false;
+		String combinerClass = "";
 		try {
 			samplingRate = Integer.parseInt(args[2]);
 			numBins = Integer.parseInt(args[3]);
-			useCombiner = Boolean.parseBoolean(args[4]);
+			if(args.length == 5)
+				combinerClass = args[4];
 		} catch(NumberFormatException e) {
 			System.err.println("Failed to parse sampling rate and/or number of bins.");
 			System.exit(-1);
@@ -40,7 +41,6 @@ public class A2_Driver {
 		// set the arguments in the config so they are available to the mapper
 		Configuration conf = new Configuration();
 		conf.set("samplingRate", String.valueOf(samplingRate));
-		conf.set("numBins", String.valueOf(numBins));
 		
 		// create a new hadoop job
 		Job medianPriceJob = new Job(conf);
@@ -54,8 +54,14 @@ public class A2_Driver {
 		// configure the map and reduce tasks
 		medianPriceJob.setMapperClass(MedianPriceMapper.class);
 		medianPriceJob.setReducerClass(MedianPriceReducer.class);
-		if(useCombiner)
+		
+		// set the combiner
+		if(combinerClass.equalsIgnoreCase("MedianPriceCombiner"))
 			medianPriceJob.setCombinerClass(MedianPriceCombiner.class);
+		else if(combinerClass.equalsIgnoreCase("TODO"))
+			medianPriceJob.setCombinerClass(MedianPriceCombiner.class);
+		
+		// set the number of "bins"
 		medianPriceJob.setNumReduceTasks(numBins);
 		
 		// configure the output settings
